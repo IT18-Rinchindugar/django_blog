8ded5cfc5bddf3663241180f43944991dbeb4105
dashk4 <dashniamdashniam@gmail.com>
2022-01-07 22:14:12 +0800

    settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1i#pzx0e=%331jqq0(off_*nck57%!md1v3+grt!@(sks9%e9)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['limitless-caverns-82924.git', '127.0.0.1']
+ALLOWED_HOSTS = ['sampledomain.com', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
